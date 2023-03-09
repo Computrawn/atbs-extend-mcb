33bf2d8ec178621bb3a7c70748a42314087f51ee
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-09 15:51:17 -0600

    Added script_example.txt file and updated notation.

--- extend_mcb.py
+++ extend_mcb.py
@@ -16,17 +16,19 @@ mcb_shelf = shelve.open("mcb")
 if len(sys.argv) == 3 and sys.argv[1].lower() == "save":
     mcb_shelf[sys.argv[2]] = pyperclip.paste()
     print(f"Saving text: {pyperclip.paste()}")
+# Delete keyword and associated text from database.
 elif len(sys.argv) == 3 and sys.argv[1].lower() == "delete":
     del mcb_shelf[sys.argv[2]]
-#    print(f"{mcb_shelf[sys.argv[2]]} has been removed from the list.")
 elif len(sys.argv) == 2:
-    # List keywords and load content.
+    # List keywords
     if sys.argv[1].lower() == "list":
         key_list = str(list(mcb_shelf.keys()))
         pyperclip.copy(key_list)
         print(key_list)
+    # Clear database
     elif sys.argv[1].lower() == "delete":
         mcb_shelf.clear()
+    # Load text associated with keyword to clipboard.
     elif sys.argv[1] in mcb_shelf:
         pyperclip.copy(mcb_shelf[sys.argv[1]])
 
